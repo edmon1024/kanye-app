675e2c4cf684fc02ef4d28857bbee6dc428c761f
eandrade <edmon.af@gmail.com>
2019-09-15 18:34:19 -0500

    new page: about

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Layout from '../components/Layout'
+import Content from '../components/Content'
+import Header from '../components/Header'
+import Card from '../components/Card'
+
+const Page = () => (
+  <Layout>
+    <Content>
+      <Header>Hello workshop</Header>
+
+      <h2>Hello world!!!</h2>
+    </Content>
+  </Layout>
+)
+
+export default Page
